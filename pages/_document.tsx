9084245fb6319150ce47f4e6384d1d6097bbf934
Ukraine At War Today <ukraine.at.war.editor@gmail.com>
2022-10-10 13:07:30 +0100

    Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ export default class MyDocument extends Document {
         <Html lang='en'>
           <Head>
             <link rel='shortcut icon' href='/favicon.ico' />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='favicon.png'
-            />
+
+            <link rel='icon' type='image/png' sizes='512x512'  href='/android-chrome-512x512' />
+            <link rel='icon' type='image/png' sizes='192x192'  href='/android-chrome-192x192' />
+            <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+            <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+            <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
 
             <link rel='manifest' href='/manifest.json' />
           </Head>
