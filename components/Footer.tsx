fe3451b498078b956dbd9f7cbc217d3a6b8a7153
Ukraine At War Today <ukraine.at.war.editor@gmail.com>
2022-10-10 23:43:04 +0100

    Contact us in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,6 +10,12 @@ export const FooterImpl: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright © {year} {config.author}</div>
+      <div className={styles.contact}>
+        Got feedback?{' '}
+        <a href="mailto:ukraine.at.war.feedback@gmail.com">
+          Let us know
+        </a>
+      </div>
     </footer>
   )
 }
