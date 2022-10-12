8074a28fcb69ce848adfa05fb522b65dd25f0865
Ukraine At War Today <ukraine.at.war.editor@gmail.com>
2022-10-12 22:09:05 +0100

    Remove word Copyright from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright © {year} {config.author}</div>
+      <div className={styles.copyright}>© {year} {config.author}</div>
       <div className={styles.contact}>
         Got feedback?{' '}
         <a href="mailto:ukraine.at.war.feedback@gmail.com">
